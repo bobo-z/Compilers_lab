e95cfdde3091048f2a0f1fdd1b32cc60b7919a49
zyb <804422812@qq.com>
2021-04-26 15:58:08 +0800

    finish symbol table

--- Code/symbol_tab.c
+++ Code/symbol_tab.c
@@ -1,6 +1,8 @@
 #include "common.h"
 
-Symbol init()
+FieldList symbol_tab[TAB_SIZE];
+
+FieldList init()
 {
     int i;
     for(i=0;i<TAB_SIZE;i++)
@@ -15,4 +17,38 @@ unsigned int hash_bkdr(char* name)
         hash = hash * seed + (*name++);
     }
     return hash%32767;
+}
+
+FieldList find(char* name)
+{
+    unsigned int key = hash_bkdr(name)%TAB_SIZE;
+    FieldList p = symbol_tab[key];
+    while(p!=NULL)
+    {
+        if(strcmp(p->name, name)==0)
+        {
+            return p;
+        }
+        p = p->tail;
+    }
+    return NULL;
+}
+
+FieldList insert(char* name, Type type)
+{
+    unsigned int key = hash_bkdr(name)%TAB_SIZE;
+    FieldList p = (FieldList)malloc(sizeof(struct FieldList_));
+    p->name = name;
+    p->type =type;
+    p->tail = NULL;
+    if(symbol_tab[key]==NULL)
+    {
+        symbol_tab[key] = p;
+    }
+    else
+    {
+        p->tail = symbol_tab[key];
+        symbol_tab[key] = p;
+    }
+    return p;
 }
--- Code/symbol_tab.h
+++ Code/symbol_tab.h
@@ -6,20 +6,10 @@
 #define NAME_LEN 30
 #define TAB_SIZE 10000
 
-typedef struct Symbol_* Symbol;
-struct Symbol_
-{
-    char name[NAME_LEN];
-    Type type;
-    Symbol next;
-};
-Symbol symbol_tab[TAB_SIZE];
-
-
-Symbol init();
-Symbol insert(char* name, Type type);
-Symbol find(char* name);
-Symbol struct_find(char* name, Type type);
+FieldList init();
+FieldList insert(char* name, Type type);
+FieldList find(char* name);
+FieldList struct_find(char* name, Type type);
 unsigned int hash_bkdr(char* name);
 
 
